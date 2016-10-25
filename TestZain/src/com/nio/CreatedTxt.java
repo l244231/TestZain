332dc4cbe9e336f25c67126532104027a84aa21a
zain.ThinkPad <l244231@gmail.com>
2016-10-25 15:05:32 +0800

    NIO,Test,large file read

--- TestZain/src/com/nio/CreatedTxt.java
+++ TestZain/src/com/nio/CreatedTxt.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 public class CreatedTxt {
 	public static void main(String[] args) {
 		String message = "上周，苹果已经对外发布邀请函，宣布将于10月27日下周五凌晨一点（北京时间）在库比提诺举行新品发布会。此次发布会的主题为“Hello again（你好，又见面了）”。\r\n";
-		File file = new File("E:\\Test\\NIO\\test.txt");
+		
+		File file = new File("E:\\Test\\NIO\\test2.txt");
 
 		try {
 			if (file.exists()) {
@@ -16,8 +17,8 @@ public class CreatedTxt {
 			}
 			file.createNewFile();
 			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-
-			for (int i = 1; i < 1000000; i++) {
+			
+			for (int i = 1; i < 10000000; i++) {
 				bw.write("行号：" + i + "；" + message);
 				if (i % 1000 == 0) {
 					bw.flush();
@@ -25,6 +26,7 @@ public class CreatedTxt {
 			}
 			bw.close();
 			bw = null;
+			System.out.println("数据生成成功");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- TestZain/src/com/nio/ReadTxtByLineNum.java
+++ TestZain/src/com/nio/ReadTxtByLineNum.java
@@ -12,38 +12,124 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Arrays;
 
 /**
- * @title 通过行号获取TXT文件中的信息 
+ * @title 通过行号获取TXT文件中的信息
  * @author Zain.Luo
  * @version 1.0<br>
- * history<br>
- * 2016年10月24日 Zain.Luo create file<br>
- * Id:ReadTxtByLineNum.java,v1.0 2016年10月24日 下午4:50:57
+ *          history<br>
+ *          2016年10月24日 Zain.Luo create file<br>
+ *          Id:ReadTxtByLineNum.java,v1.0 2016年10月24日 下午4:50:57
  */
 public class ReadTxtByLineNum {
-	private static int length = 1024*1024*10;
+	/**
+	 * 每次读取文件内容大小
+	 */
+	private static int length = 1024 * 10;
+
 	public static void main(String[] args) {
-		
-	}
-	
-	public static void readTxt(){
-		File file = new File("E:\\Test\\NIO\\test.txt");
-		
+		long start = System.currentTimeMillis();
+		String result;
 		try {
-			FileChannel fc = new RandomAccessFile(file, "r").getChannel();
-			MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, 0, length);
-			ByteBuffer buffer = ByteBuffer.allocate(length);
-			int readCount;
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+			result = readTxt("E:\\Test\\NIO\\test2.txt", 77777);
+			System.out.println(result);
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+		System.out.println(System.currentTimeMillis() - start);
 	}
-	
+
+	/**
+	 * @Title: readTxt
+	 * @author: Zain.Luo
+	 * @Description: 读取文件
+	 * @param filePath
+	 *            文件路径+文件名
+	 * @param lineNum
+	 *            获取内容行数
+	 * @throws Exception
+	 * @return String 结果
+	 * @throws @history:
+	 *             2016年10月25日 created
+	 */
+	public static String readTxt(String filePath, long lineNum) throws Exception {
+		File file = new File(filePath);
+		String result = null;
+		try {
+			FileChannel fc = new RandomAccessFile(file, "r").getChannel();
+
+			ByteBuffer buffer = ByteBuffer.allocate(length);
+			int readCount;
+			int lines = 0;
+			int bufferCount = 0;
+
+			out: while (0 != (readCount = fc.read(buffer))) {
+				buffer.flip();
+				for (int i = 0; i < readCount; i++) {
+
+					/* 行数判断，捕获到\n时，判断为一行 */
+					if ((int) buffer.get(i) == '\n') {
+						lines++;
+						if (lines == lineNum - 1) {
+							result = getResult(fc, bufferCount * length + i + 1);
+							break out;
+						}
+					}
+				}
+				bufferCount++;
+			}
+			fc.close();
+			if (result == null) {
+				throw new Exception("无法找到对应行数内容，请确认行数是否正确");
+			}
+			return result;
+
+		} catch (FileNotFoundException e) {
+			throw new Exception("文件：不存在");
+		} catch (IOException e) {
+			throw new Exception("文件读取发生异常，请确保文件正确或具有相应权限");
+		}
+	}
+
+	/**
+	 * @Title: getResult
+	 * @author: Zain.Luo
+	 * @Description: 从上一行结尾开始读取一行数
+	 * @param bufferCount
+	 *            上一行结果数据位置
+	 * @return
+	 * @throws IOException
+	 * @return String
+	 * @throws @history:
+	 *             2016年10月25日 created
+	 */
+	public static String getResult(FileChannel fc, long bufferCount) throws IOException {
+		byte[] result = new byte[length];
+		String resultStr;
+		MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, bufferCount, length);
+
+		/*判断是否读取完一行数据，读取完，将不进行读取数据*/
+		for (int i = 0; i < length; i++) {
+			if (mbb.get(i) == '\n') {
+				bufferCount = 0;
+				result = Arrays.copyOfRange(result, 0, i);
+				break;
+			}
+			result[i] = mbb.get(i);
+		}
+		mbb.clear();
+
+		/* 判断是否读取完一行数据，可能出现：某一行数据太大，大于缓冲区大小length，此时需要进行再次读取 */
+		resultStr = new String(result, "UTF-8");
+		if (resultStr == null || result.equals("")) {
+			return null;
+		} else if(bufferCount != 0){
+			resultStr = resultStr + getResult(fc, bufferCount + length);
+		}
+		return resultStr;
+	}
+
 }
